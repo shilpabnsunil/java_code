482d3ff1a9ece6de133e0552bb0b12d2aa1a5d36
shilpa sunil <shilpabnsunil@gmail.com>
2024-09-12 16:05:32 +0530

    sending one file

---
+++ DataTypesVaribles.java
@@ -0,0 +1,31 @@
+package basics;
+
+public class DataTypesVaribles {
+	
+	public static void main(String[] args) {
+		
+        byte a=10; //Declaration and Initialization of a variable 
+		System.out.println(a);
+		short b=20;//Declaration and Initialization of a variable 
+		System.out.println(b);
+		int c=30;//Declaration and Initialization of a variable 
+		System.out.println(c);
+		long d=40;//Declaration and Initialization of a variable 
+		System.out.println(d);
+		float e=2.6f;//Declaration and Initialization of a variable 
+		System.out.println(e);
+		double f=4.2;//Declaration and Initialization of a variable 
+		System.out.println(f);
+		char g='F';//Declaration and Initialization of a variable 
+		System.out.println(g);
+		boolean h=true;//Declaration and Initialization of a variable 
+		System.out.println(h);
+		boolean i=false;//Declaration and Initialization of a variable 
+		System.out.println(i);
+		String name="Shilpa B N";//Declaration and Initialization of a variable 
+		System.out.println(name);
+	}
+	
+	
+
+}
